557a62bda4f3a6577f3764b854bdcf2cb8da548f
Lotis <guillaume.lotis@inventhys.com>
2022-10-21 17:30:04 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Guillaume Lotis on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
